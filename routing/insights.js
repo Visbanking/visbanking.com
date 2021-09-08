3cd81409d0253b97f93df4426e829f04d8349b54
Jorge Gallego <gallegojorge908@gmail.com>
2021-09-08 10:38:19 -0500

    Changed insights routing

--- routing/insights.js
+++ routing/insights.js
@@ -9,8 +9,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:article_id", (req, res, next) => {
-    console.log(req.params);
-    next();
+    res.send(req.params);
     // Query the database for an article with id = article_id
     // If query return an object, render a template passing the object to show the article
     // If query doesn't return an object, set status code as 404 and redirect
