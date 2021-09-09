092059bb71b5cb5b61808c86e0162ccd09ecae00
Jorge Gallego <gallegojorge908@gmail.com>
2021-09-09 08:15:13 -0500

    Added server logic for /subscribe path

--- routing/subscribe.js
+++ routing/subscribe.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const existence = require("email-existence");
+// const existence = require("email-existence");
+// const client = require("@mailchimp/mailchimp_marketing");
 const router = express.Router();
 
-router.get("/subscribe", (req, res) => {
+router.use(bodyParser.urlencoded({extended: true}));
+
+// client.setConfig({
+//     apiKey: '<MailChimp API Key>',
+//     server: '<MailChimp List Server>'
+// });
+
+router.get("/", (req, res) => {
     res.render("subscribe", {
         title: "Subscribe to our Newsletter - Visbanking",
         path: "/subscribe"
     });
+});
+
+router.post("/", (req, res) => {
+    res.send(req.body);
 });
 
 module.exports = router;
