d021ef21d0753d8b782c0c3f5d86150f3aeb0837
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-02 09:18:41 -0500

    Created route handlers for /contact

--- routing/contact.js
+++ routing/contact.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const existence = require("email-existence");
+const connection = require("./dbconnection");
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({extended: true}));
@@ -18,11 +19,30 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
     const name = req.body.name, email = req.body.email, subject = req.body.subject, message = req.body.message;
     existence.check(email, (err, response) => {
-        if (err || !response) {
+        if (err) {
             console.error(err);
+            error = 'There was a problem. Please try again.';
+        } else if (!response) {
             error = `${email} doesn't exist`;
+        } else {
+            connection.query(`INSERT INTO Contacts (Name, Email, Subject, Message) VALUES ('${name}', '${email}', '${subject}', '${message}');`, (err, results, fields) => {
+                if (err) {
+                    console.error(err);
+                    res.redirect("/error");
+                } else {
+                    res.redirect("/contact/success");
+                }
+            });
         }
-    })
-})
+        res.redirect("/contact");
+    });
+});
+
+router.get("/success", (req, res) => {
+    res.render("success", {
+        title: "Success - Visbanking",
+        path: "/contact/success"
+    });
+});
 
 module.exports = router;
