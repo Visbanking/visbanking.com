f353e23892d0fbd2a358ebd3042ce7831e5f46e4
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-20 13:08:33 -0500

    Created /insights server logic and view

--- routing/insights.js
+++ routing/insights.js
@@ -3,17 +3,25 @@ const router = express.Router();
 const connection = require("./dbconnection");
 
 router.get("/", (req, res) => {
-    res.render("insights", {
-        title: "Insights - Visbanking",
-        path: "/insights"
-    });
+	connection.query("SELECT * FROM Insights;", (err, results, fields) => {
+		if (err) {
+			console.error(err);
+			res.redirect("/error");
+		} else {
+			res.render("insights", {
+				title: "Insights - Visbanking",
+				path: "/insights",
+                posts: results
+			});
+        }
+	});
 });
 
 router.get("/:article_id", (req, res, next) => {
-    res.send(req.params);
-    // Query the database for an article with id = article_id
-    // If query return an object, render a template passing the object to show the article
-    // If query doesn't return an object, set status code as 404 and redirect
+	res.send(req.params);
+	// Query the database for an article with id = article_id
+	// If query return an object, render a template passing the object to show the article
+	// If query doesn't return an object, set status code as 404 and redirect
 });
 
 module.exports = router;
