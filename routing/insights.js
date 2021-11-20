9674bd2c1004e3b4b4c780f75801c1a7cceaf2c4
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-20 13:09:06 -0500

    Updated /insights server code

--- routing/insights.js
+++ routing/insights.js
@@ -17,11 +17,21 @@ router.get("/", (req, res) => {
 	});
 });
 
-router.get("/:article_id", (req, res, next) => {
-	res.send(req.params);
-	// Query the database for an article with id = article_id
-	// If query return an object, render a template passing the object to show the article
-	// If query doesn't return an object, set status code as 404 and redirect
+router.get("/article/:article_id", (req, res, next) => {
+    connection.query(`SELECT * FROM Insights WHERE ID = '${req.params.article_id}';`, (err, results, fields) => {
+        if (err) {
+            console.error(err);
+            res.redirect("/error");
+        } else if (results.length === 0) {
+            res.redirect("/insights");
+        } else {
+            res.render("insight", {
+                title: results[0].Title,
+                path: req.originalUrl,
+                post: results[0]
+            });
+        }
+    });
 });
 
 module.exports = router;
