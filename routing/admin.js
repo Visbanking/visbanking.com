7dfb5bebd3eda506d867bca0021a261f5dde94b6
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-25 14:05:33 -0500

    Created server-side logic for /admin routes

--- routing/admin.js
+++ routing/admin.js
@@ -3,23 +3,57 @@ const connection = require("./dbconnection");
 const hash = require("hash.js");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const lodash = require("lodash");
+const multer = require("multer");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const router = Router();
 
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(cookieParser());
 
-let error;
+let error, message;
+const insightStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "..", "static", "images", "insights"));
+    }, 
+    filename: (req, file, cb) => {
+        cb(null, `${lodash.camelCase(req.body.title)}.jpg`);
+    }
+});
+const memberStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        debug(null, path.join(__dirname, "..", "static", "images", "members"));
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${lodash.camelCase(req.body.name)}.jpg`);
+    }
+});
+
+const insight = multer({ storage:insightStorage });
+const member = multer({ storage:memberStorage });
 
 router.all("/", (req, res) => {
     res.redirect("/admin/login");
 });
 
 router.get("/login", (req, res) => {
-    res.render("admin", {
-        title: "Admin Login - Visbanking",
-        error: error
-    });
-    error = "";
+    if (req.cookies.admin) {
+        connection.query(`SELECT COUNT(*) FROM Admins WHERE Username = '${req.cookies.admin}';`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                res.redirect("/error");
+            } else if (results[0]["COUNT(*)"] !== 0) {
+                res.redirect("/admin/dashboard");
+            }
+        });
+    } else {
+        res.render("admin", {
+            title: "Admin Login - Visbanking",
+            error: error
+        });
+        error = "";
+    }
 });
 
 router.post("/login", (req, res) => {
@@ -31,12 +65,12 @@ router.post("/login", (req, res) => {
         } else if (results.length === 0) {
             res.redirect("/");
         } else if (results[0].Password !== pass) {
-            error = "The email or password entered were invalid";
+            error = "The username or password entered were invalid";
             res.redirect("/admin/login");
         } else {
             res.cookie('admin', username, {
                 httpOnly: true,
-                expires: new Date(Date.now() + 8_640_000)
+                expires: new Date(Date.now() + 8640000)
             });
             res.redirect("/admin/dashboard");
         }
@@ -56,7 +90,141 @@ router.get("/dashboard", (req, res) => {
             } else {
                 res.render("dashboard", {
                     title: "Admin Dashboard - Visbanking",
-                    admin: results[0]
+                    admin: results[0],
+                    message: message
+                });
+                message = "";
+            }
+        });
+    }
+});
+
+router.post("/dashboard/profile", (req, res) => {
+    const cb = (err, results, fields) => {
+        if (err) {
+            console.error(err);
+            message = "Profile information couldn't be updated. Please try again.";
+            res.redirect("/admin/dashboard");
+        } else {
+            res.clearCookie("admin");
+            res.redirect("/admin");
+        }
+    }
+    if (req.body.username.trim() !== '' && req.body.pass.trim() !== '') {
+        const username = req.body.username.trim(), pass = hash.sha512().update(req.body.pass.trim()).digest("hex");
+        connection.query(`UPDATE Admins SET Username = '${username}', Password = '${pass}' WHERE ID = (SELECT ID FROM Admins WHERE Username = '${req.cookies.admin}');`, cb(err, results, fields));
+    } else if (req.body.pass.trim() !== '') {
+        const pass = hash.sha512().update(req.body.pass.trim()).digest("hex");
+        connection.query(`UPDATE Admins SET Password = '${pass}' WHERE ID = (SELECT ID FROM Admins WHERE Username = '${req.cookies.admin}');`, cb(err, results, fields));
+    } else if (req.body.username.trim() !== '') {
+        const username = req.body.username.trim();
+        connection.query(`UPDATE Admins SET Username = '${username}' WHERE ID = (SELECT ID FROM Admins WHERE Username = '${req.cookies.admin}');`, cb(err, results, fields));
+    }
+});
+
+router.post("/dashboard/admins", (req, res) => {
+    const action = req.body.action;
+    if (action === "Add admin") {
+        const username = req.body.username.trim(), pass = hash.sha512().update(req.body.pass.trim()).digest("hex");
+        connection.query(`INSERT INTO Admins (Username, Password) VALUES ('${username}', '${pass}');`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                message = "New admin couldn't be created. Please try again.";
+                res.redirect("/admin/dashboard");
+            } else {
+                message = "New admin created successfully.";
+                res.redirect("/admin/dashboard");
+            }
+        });
+    } else if (action === "Delete admin") {
+        connection.query(`SELECT ID FROM Admins WHERE Username = '${req.body.username}';`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                message = "Admin couldn't be deleted. Please try again.";
+                res.redirect("/admin/dashboard");
+            } else {
+                const id = results[0].ID;
+                connection.query(`DELETE FROM Admins WHERE ID = '${id}';`, (err, results, fields) => {
+                    if (err) {
+                        console.error(err);
+                        message = "Admin couldn't be deleted. Please try again.";
+                        res.redirect("/admin/dashboard");
+                    } else {
+                        message = "Admin deleted successfully.";
+                        res.redirect("/admin/dashboard");
+                    }
+                });
+            }
+        });
+    }
+});
+
+router.post("/dashboard/insights", insight.single('image'), (req, res) => {
+    const action = req.body.action;
+    if (action === "Add insight") {
+        connection.query(`INSERT INTO Insights VALUES ('${uuidv4()}', '${req.body.title}', '${req.body.body}', '/images/insights/${req.file.filename}', '${req.body.topics}');`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                message = "Insight couldn't be created. Please try again.";
+                res.redirect("/admin/dashboard");
+            } else {
+                message = "Insight created successfully.";
+                res.redirect("/admin/dashboard");
+            }
+        });
+    } else if (action === "Delete insight") {
+        connection.query(`SELECT ID FROM Insights WHERE Title = '${req.body.title}';`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                message = "Insight couldn't be deleted. Please try again";
+                res.redirect("/admin/dashboard");
+            } else {
+                const id = results[0].ID;
+                connection.query(`DELETE FROM Insights WHERE ID = '${id}';`, (err, results, fields) => {
+                    if (err) {
+                        console.error(err);
+                        message = "Insight couldn't be deleted. Please try again";
+                        res.redirect("/admin/dashboard");
+                    } else {
+                        message = "Insight deleted successfully.";
+                        res.redirect("/admin/dashboard");
+                    }
+                });
+            }
+        });
+    }
+});
+
+router.post("/dashboard/members", member.single('photo'), (req, res) => {
+    const action = req.body.action;
+    if (action === "Add member") {
+        connection.query(`INSERT INTO Members (Name, Photo, Bio, LinkedIn) VALUES ('${req.body.name}', '/images/members/${req.file.filename}', '${req.body.bio}', '${req.body.linkedin}');`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                message = "Member couldn't be created. Please try again.";
+                res.redirect("/admin/dashboard");
+            } else {
+                message = "Member created successfully.";
+                res.redirect("/admin/dashboard");
+            }
+        });
+    } else if (action === "Delete member") {
+        connection.query(`SELECT ID FROM Members WHERE Name = '${req.body.name}';`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                message = "Member couldn't be deleted. Please try again.";
+                res.redirect("/admin/dashboard");
+            } else {
+                const id = results[0].ID;
+                connection.query(`DELETE FROM Members WHERE ID = '${id}';`, (err, results, fields) => {
+                    if (err) {
+                        console.error(err);
+                        message = "Member couldn't be deleted. Please try again.";
+                        res.redirect("/admin/dashboard");
+                    } else {
+                        message = "Member deleted successfully.";
+                        res.redirect("/admin/dashboard");
+                    }
                 });
             }
         });
