67a58ccc7d601ce01a93fe7d459e4d142cdad700
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-27 08:43:42 -0500

    Updated /insights view

--- static/js/insights.js
+++ static/js/insights.js
@@ -1,3 +1,37 @@
+Vue.createApp({
+    data() {
+        return {
+            business: true,
+            banks: false,
+            financial: false,
+            market: false,
+            politics: false
+        }
+    },
+    methods: {
+        showBusiness() {
+            this.banks = this.financial = this.market = this.politics = false;
+            this.business = true;
+        },
+        showBanks() {
+            this.business = this.financial = this.market = this.politics = false;
+            this.banks = true;
+        },
+        showFinancial() {
+            this.banks = this.business = this.market = this.politics = false;
+            this.financial = true;
+        },
+        showMarket() {
+            this.banks = this.financial = this.business = this.politics = false;
+            this.market = true;
+        },
+        showPolitics() {
+            this.banks = this.financial = this.market = this.business = false;
+            this.politics = true;
+        }
+    }
+}).mount("#latest");
+
 document.querySelectorAll("article.post").forEach(post => {
     post.addEventListener("click", () => {
         post.children[post.children.length-1].click();
