6cc1e545d55eecc36864bddc9be7faaaecf626fc
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-27 22:03:56 -0500

    Parsed HTML content within JS file

--- static/js/insight.js
+++ static/js/insight.js
@@ -1,3 +1,5 @@
+document.querySelector("article main").innerHTML = document.querySelector("article main").innerText;
+
 document.querySelectorAll("article main a").forEach(link => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'noreferrer noopener nofollow');
