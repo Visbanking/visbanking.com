6ef959f73b2ff8038afaa06ea980993f35945885
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-27 21:54:33 -0500

    Implemented categories and 'Top Posts'

--- routing/insights.js
+++ routing/insights.js
@@ -4,15 +4,35 @@ const router = express.Router();
 const connection = require("./dbconnection");
 
 router.get("/", (req, res) => {
-	connection.query("SELECT * FROM Insights;", (err, results, fields) => {
+	connection.query("SELECT * FROM Insights ORDER BY Views DESC;", (err, results, fields) => {
 		if (err) {
 			console.error(err);
 			res.redirect("/error");
 		} else {
+            const business = results.filter(result => {
+                return result.Topics.split(", ").includes("business")
+            });
+            const banks = results.filter(result => {
+                return result.Topics.split(", ").includes("banks")
+            });
+            const financial = results.filter(result => {
+                return result.Topics.split(", ").includes("financial")
+            });
+            const market = results.filter(result => {
+                return result.Topics.split(", ").includes("market")
+            });
+            const politics = results.filter(result => {
+                return result.Topics.split(", ").includes("politics")
+            });
 			res.render("insights", {
 				title: "Insights - Visbanking",
 				path: "/insights",
-                posts: results
+                posts: results,
+                business,
+                banks,
+                financial,
+                market,
+                politics
 			});
         }
 	});
@@ -26,6 +46,12 @@ router.get("/insight/:article_id", (req, res) => {
         } else if (results.length === 0) {
             res.redirect("/insights");
         } else {
+            connection.query(`UPDATE Insights SET Views = ${results[0].Views+1} WHERE ID = '${req.params.article_id}';`, (err, results, fields) => {
+                if (err) {
+                    console.error(err);
+                    res.redirect("/error");
+                }
+            });
             const body = [], topics = [];
             results[0].Body.split("  ").forEach(par => {
                 body.push(`<p>${par}</p>`);
