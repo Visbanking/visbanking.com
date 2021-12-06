4d3dde1be79ff47d1b99494fe0957a488111457f
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-06 07:36:43 -0500

    Added security Response Headers

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.use(cors());
 app.set("view engine", "pug");
 
 app.use((req, res, next) => {
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('X-Frame-Options', 'DENY');
     res.removeHeader('X-Powered-By');
     next();
 });
