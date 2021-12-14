c9b15724038ba57631e3aa5eae86fa129886b0c4
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-14 11:48:13 -0500

    Added ability to upload and update profile picture

--- routing/user.js
+++ routing/user.js
@@ -3,12 +3,24 @@ const router = express.Router();
 const { urlencoded } = require("body-parser"); 
 const cookieParser = require("cookie-parser");
 const hash = require("hash.js");
+const multer = require("multer");
+const path = require("path");
+const lodash = require("lodash");
 const connection = require("./dbconnection");
 
 router.use(urlencoded({extended: true}));
 router.use(cookieParser());
 
-var error;
+let error;
+const userStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "..", "static", "images", "users"));
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${lodash.camelCase(req.cookies.user.split("@")[0])}.jpg`);
+    }
+});
+const user = multer({ storage:userStorage });
 
 router.get("/", (req, res) => {
     res.render("users", {
@@ -28,33 +40,68 @@ router.get("/:email", (req, res) => {
             res.render("user", {
                 title: `${results[0].FirstName} ${results[0].LastName} | Users - Visbanking`,
                 userInfo: results[0],
-                user: req.cookies.user||""
+                user: req.cookies.user||"",
+                access: req.cookies.user===results[0].Email
             });
         }
     });
 });
 
-router.post("/:email", (req, res) => {
-    const fname = req.body.name.split(" ")[0], lname = req.body.name.split(" ")[1];
-    if (!fname || !lname) {
-        return res.redirect(`/users/${req.params.email}`);
+router.post("/:email", user.single('image'), (req, res) => {
+    if (req.body.name.split(" ").length > 1 && req.file) {
+        const fname = req.body.name.split(" ")[0], lname = req.body.name.split(" ")[1];
+        connection.query(`SELECT ID FROM Users WHERE Email = '${req.params.email}';`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                res.redirect("/error");
+            } else {
+                const id = results[0].ID;
+                connection.query(`UPDATE Users SET FirstName = '${fname}', LastName = '${lname}', Image = '/images/users/${req.file.filename}' WHERE ID = ${id};`, (err, results, fields) => {
+                    if (err) {
+                        console.error(err);
+                        res.redirect("/error");
+                    } else {
+                        res.redirect(`/users/${req.params.email}`);
+                    }
+                });
+            }
+        });
+    } else if (!req.file) {
+        const fname = req.body.name.split(" ")[0], lname = req.body.name.split(" ")[1];
+        connection.query(`SELECT ID FROM Users WHERE Email = '${req.params.email}';`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                res.redirect("/error");
+            } else {
+                const id = results[0].ID;
+                connection.query(`UPDATE Users SET FirstName = '${fname}', LastName = '${lname}' WHERE ID = ${id};`, (err, results, fields) => {
+                    if (err) {
+                        console.error(err);
+                        res.redirect("/error");
+                    } else {
+                        res.redirect(`/users/${req.params.email}`);
+                    }
+                });
+            }
+        });
+    } else if (!(req.body.name.split(" ").length > 1)) {
+        connection.query(`SELECT ID FROM Users WHERE Email = '${req.params.email}';`, (err, results, fields) => {
+            if (err) {
+                console.error(err);
+                res.redirect("/error");
+            } else {
+                const id = results[0].ID;
+                connection.query(`UPDATE Users SET Image = '/images/users/${req.file.filename}' WHERE ID = ${id};`, (err, results, fields) => {
+                    if (err) {
+                        console.error(err);
+                        res.redirect("/error");
+                    } else {
+                        res.redirect(`/users/${req.params.email}`);
+                    }
+                });
+            }
+        });
     }
-    connection.query(`SELECT ID FROM Users WHERE Email = '${req.params.email}';`, (err, results, fields) => {
-        if (err) {
-            console.error(err);
-            res.redirect("/error");
-        } else {
-            const id = results[0].ID;
-            connection.query(`UPDATE Users SET FirstName = '${fname}', LastName = '${lname}' WHERE ID = ${id};`, (err, results, fields) => {
-                if (err) {
-                    console.error(err);
-                    res.redirect("/error");
-                } else {
-                    res.redirect(`/users/${req.params.email}`);
-                }
-            });
-        }
-    });
 });
 
 router.get("/:email/update", (req, res) => {
--- static/js/user.js
+++ static/js/user.js
@@ -1,10 +1,25 @@
+document.querySelector("#profile").addEventListener("click", (event) => {
+    document.querySelector(`#${event.target.id} + input[type='file']`).classList.toggle("show");
+    if (document.querySelector(`#${event.target.id} + input[type='file']`).classList.contains("show")) {
+        document.querySelector(`#${event.target.id} + input[type='file']`).click();
+        event.target.title = "";
+    } else event.target.title = "Update Profile Picture";
+});
+
+document.querySelector("input[type='file']").addEventListener("input", (event) => {
+    if (event.target.value.includes("C:"))
+        document.querySelector(`input[type="submit"]`).classList.add("show");
+    else
+        document.querySelector(`input[type="submit"]`).classList.remove("show");
+});
+
 document.querySelector("i.bi-pencil-square").addEventListener("click", (event) => {
     document.querySelector(`#${event.target.id} + input`).classList.toggle("show");
     document.querySelector(`#${event.target.id} + input`).focus();
 });
 
 document.querySelector("input[name='name']").addEventListener("keydown", (event) => {
-    if (event.target.value.trim().includes(" ")) {
+    if (event.target.value.split(" ").length === 2) {
         document.querySelector(`input[type="submit"]`).classList.add("show");
     } else {
         document.querySelector(`input[type="submit"]`).classList.remove("show");
