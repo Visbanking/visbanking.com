8c8b72a5ae76fe3f03d3f21b501114e764780d2c
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-15 12:08:21 -0500

    Added required functionality to /sitemap.xml route

--- routing/files.js
+++ routing/files.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const path = require("path");
+const connection = require("./dbconnection");
+const { writeFile } = require("fs");
 const router = express.Router();
 
 router.get("/robots.txt", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "robots.txt"));
 });
 
-router.get("/sitemap.xml", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "sitemap.xml"));
+router.get("/sitemap.xml", async (req, res) => {
+    let sitemap = '<?xml version="1.1" encoding="UTF-8"?>\n';
+    sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+    const static = ["/", "/about", "/about/team", "/services", "/insights", "/contact", "/subscribe", "/buy", "/pricing", "/login", "/signup", "/terms", "/privacy", "/cookies", "/disclaimer"];
+    for (let path of static) {
+        sitemap += `\t<url><loc>https://visbanking.com${path}</loc></url>\n`;
+    }
+    connection.query('SELECT ID FROM Insights;', (err, insights, fields) => {
+        if (err) throw err
+        connection.query('SELECT Email FROM Users;', (err, users, fields) => {
+            if (err) throw err
+            for (let insight of insights) {
+                sitemap += `\t<url><loc>https://visbanking.com/insights/insight/${insight.ID}</loc></url>\n`;
+            }
+            for (let user of users) {
+                sitemap += `\t<url><loc>https://visbanking.com/users/user/${user.Email}</loc></url>\n`;
+            }
+            sitemap += '</urlset>';
+            writeFile(path.join(__dirname, "..", "sitemap.xml"), sitemap, () => {
+                res.sendFile(path.join(__dirname, "..", "sitemap.xml"));
+            });
+        });
+    });
 });
 
 router.all(["*/robots", "*/robots.txt"], (req, res) => {
