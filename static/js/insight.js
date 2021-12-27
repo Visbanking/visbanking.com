48576cfc4a028d9254092056634eebb56229fbcc
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-27 15:59:07 -0500

    Implemented desktop view for individual insight pages

--- static/js/insight.js
+++ static/js/insight.js
@@ -4,3 +4,12 @@ document.querySelectorAll("article main a").forEach(link => {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'noreferrer noopener nofollow');
 });
+
+document.querySelector("#share #linkedin").setAttribute('href', `https://linkedin.com/cws/share/?url=${window.location.href}`);
+document.querySelector("#share #facebook").setAttribute('href', `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`);
+document.querySelector("#share #twitter").setAttribute('href', `https://twitter.com/intent/tweet?text=${document.title}&url=${window.location.href}`);
+
+document.querySelectorAll("#related .intro").forEach(intro => {
+    intro.innerHTML = intro.innerText;
+    intro.innerText = intro.innerText.split("\n\n")[0];
+});
