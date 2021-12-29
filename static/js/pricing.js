2849c080b976b7e94adc1df7c59154fa9027f539
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-29 07:55:42 -0500

    Fixed issue with card height

--- static/js/pricing.js
+++ static/js/pricing.js
@@ -1,3 +1,9 @@
+const maxCardHeight = document.querySelector(".plan#enterprise").offsetHeight;
+
+document.querySelectorAll("aside.plan").forEach(card => {
+    card.style.height = `${maxCardHeight}px`
+});
+
 document.querySelectorAll(".card-footer button a").forEach(link => {
     link.addEventListener("click", () => {
         window.location.href = link.getAttribute("href");
