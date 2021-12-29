e3cf8e32c0d52d615e45a764cd887da766d4d0a0
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-29 14:00:11 -0500

    Updated homepage view and related styles and front-end JavaScript to match design

--- static/js/index.js
+++ static/js/index.js
@@ -1,64 +1,11 @@
-let timer = setInterval(() => {
-    intro.carouselRight();
-}, 10000);
-
-const intro = Vue.createApp({
-    data() {
-        return {
-            carousel: 1, 
-        }
-    },
-    methods: {
-        carouselLeft() {
-            this.carousel===1?this.carousel=3:this.carousel--;
-            clearInterval(timer);
-            timer = setInterval(() => {
-                this.carouselRight();
-            }, 10000);
-        },
-        carouselRight() {
-            this.carousel===3?this.carousel=1:this.carousel++;
-            clearInterval(timer);
-            timer = setInterval(() => {
-                this.carouselRight();
-            }, 10000);
-        }
-    }
-}).mount("#intro");
-
-const capabilities = Vue.createApp({
-    data() {
-        return {
-            tabs: [true, false, false]
-        }
-    },
-    methods: {
-        show(tab) {
-            this.tabs[this.tabs.indexOf(true)] = false;
-            if (tab === "about") {
-                this.tabs[0] = !this.tabs[0];
-            } else if (tab === "services") {
-                this.tabs[1] = !this.tabs[1];
-            } else if (tab === "insights") {
-                this.tabs[2] = !this.tabs[2];
-            }
-        }
-    }
-}).mount("#capabilities");
-
-document.querySelector("#newsletter aside a").addEventListener("mouseenter", () => {
-    document.querySelector("#newsletter aside a i").classList.add("active");
-});
-
-document.querySelector("#newsletter aside a").addEventListener("mouseleave", () => {
-    document.querySelector("#newsletter aside a i").classList.remove("active");
-});
-
-document.querySelectorAll("#insights article").forEach((article) => {
-    article.addEventListener("mouseenter", () => {
-        article.classList.add("active");
+document.querySelectorAll("#featured .post").forEach(post => {
+    post.addEventListener("click", () => {
+        post.children[post.children.length-1].click();
     });
-    article.addEventListener("mouseleave", () => {
-        article.classList.remove("active");
+});
+
+document.querySelectorAll("#latest .post").forEach(post => {
+    post.addEventListener("click", () => {
+        post.children[post.children.length-1].click();
     });
 });
