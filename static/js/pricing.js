0ecfe94c2aa3bbda2c5d600cc10fe76bc44db3c6
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-29 14:04:19 -0500

    Deleted redundant JavaScript code

--- static/js/pricing.js
+++ static/js/pricing.js
@@ -1,18 +1,5 @@
-const maxCardHeight = document.querySelector(".plan#enterprise").offsetHeight;
+const maxCardHeight = document.querySelector(".plan#enterprise").offsetHeight + 10;
 
 document.querySelectorAll("aside.plan").forEach(card => {
     card.style.height = `${maxCardHeight}px`
-});
-
-document.querySelectorAll(".card-footer button a").forEach(link => {
-    link.addEventListener("click", () => {
-        window.location.href = link.getAttribute("href");
-    });
-});
-
-document.querySelectorAll(".card .card-footer button").forEach(button => {
-    button.addEventListener("click", (event) => {
-        event.preventDefault();
-        button.children[0].click();
-    });
 });
