41d6cedfc138d887eb06a00d66f19493799ef41c
Jorge Gallego <gallegojorge908@gmail.com>
2021-12-30 18:33:43 -0500

    Updated success view after successful payment processing

--- static/js/success.js
+++ static/js/success.js
@@ -1,5 +1,5 @@
 if (window.location.search.includes("session_id")) {
-    setTimeout(() => { window.location.replace(`/signup?tier=${document.querySelector("#tier").innerText.trim().toLowerCase()}`); }, 5000);
+    setTimeout(() => { window.location.replace("/login"); }, 5000);
 } else {
     setTimeout(() => { window.location.replace("/"); }, 10000);
 }
