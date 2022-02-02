193d3f8b755c0ab224b0b6901efe05081cf758d4
Jorge Gallego <gallegojorge908@gmail.com>
2022-02-02 10:11:36 -0500

    Update view logic for insight creation page inside admin dashboard to include author, topic and tags, and related CSS and JavaScript files

--- static/js/create.js
+++ static/js/create.js
@@ -1,4 +1,24 @@
+document.querySelector("input#image").addEventListener("change", (event) => {
+    const [file] = event.target.files;
+    if (file) {
+        document.querySelector("img").src = URL.createObjectURL(file);
+    }
+})
+
+document.querySelector("textarea").addEventListener("input", (event) => {
+    if (event.target.value !== "") {
+        document.querySelector("input#bodyFile").disabled = true;
+        document.querySelector("input#bodyFile").title = 'Clear the \'BODY\' section to upload file';
+    } else {
+        document.querySelector("input#bodyFile").disabled = false;
+        document.querySelector("input#bodyFile").title = 'Choose file to upload';
+        document.querySelector("input#bodyFile").value = '';
+    }
+});
+
 document.querySelector("input#bodyFile").addEventListener("change", async (event) => {
     const text = await event.target.files[0].text();
     document.querySelector("textarea").value = text;
+    document.querySelector("input#bodyFile").disabled = true;
+    document.querySelector("input#bodyFile").title = 'Clear the \'BODY\' section to upload file';   
 });
