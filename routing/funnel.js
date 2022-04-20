0d75736a124332d4ad40de4979b3fdf560668156
Jorge Gallego <gallegojorge908@gmail.com>
2022-04-20 11:48:53 -0500

    Added Back End and view logic implementation for A/B testing on News Digest squeeze page

--- routing/funnel.js
+++ routing/funnel.js
@@ -15,35 +15,31 @@ client.setConfig({
 
 const verifier = new EmailVerifier(10000);
 
-router.get("/newsdigest", (req, res) => {
-    res.render("funnel/newsDigest", {
-        title: 'Subscribe to our News Digest - Visbanking',
-        path: req.originalUrl
-    });
+router.get("/newsDigest", (req, res) => {
+	res.redirect(`/funnel/newsDigest/${["a", "b"][Math.floor(Math.random() * 2)]}`);
 });
 
 router.post("/newsdigest", (req, res) => {
-    const { fname, lname, email, company } = req.body;
+    const { fname, email } = req.body;
     verifier.verify(email)
 	.then(response => {
         if (response) {
+			const data = { FNAME: fname };
+			if (req.body.lname) data.LNAME = req.body.lname;
+			if (req.body.company) data.COMPANY = req.body.company;
 			let new_client = {
 				members: [
 					{
 						email_address: email,
 						status: "subscribed",
-						merge_fields: {
-							FNAME: fname,
-							LNAME: lname,
-							COMPANY: company
-						}
+						merge_fields: data
 					},
 				],
 			};
 			const run = async () => {
 				const response = await client.lists.batchListMembers("6a0268299e", new_client);
 				if (response.error_count === 0) {
-					connection.query(`INSERT INTO NewsDigest (FirstName, LastName, Email, Company) VALUES ('${fname}', '${lname}', '${email}', '${company}');`, (err, results, fields) => {
+					connection.query(`INSERT INTO NewsDigest (FirstName, LastName, Email, Company) VALUES ('${fname}', '${req.body.lname || ''}', '${email}', '${req.body.company || ''}');`, (err, results, fields) => {
 						res.redirect("/funnel/newsdigest/success");
 					});
 				} else {
@@ -77,6 +73,16 @@ router.get("/newsdigest/failure", (req, res) => {
     res.render("failure", {
         title: "Failure - Visbanking",
         path: req.originalUrl
+    });
+});
+
+router.get("/newsdigest/:test_id", (req, res) => {
+	const test_id = req.params.test_id;
+	if (["a", "b"].indexOf(test_id) === -1) return res.redirect("/funnel/newsdigest/a");
+    res.render("funnel/newsDigest", {
+        title: 'Subscribe to our News Digest - Visbanking',
+        path: req.originalUrl,
+		test_id
     });
 });
 
