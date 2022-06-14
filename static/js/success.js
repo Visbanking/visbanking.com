8d8e59dee8a77c57c1e9a8903499babca48b4469
Jorge Gallego <gallegojorge908@gmail.com>
2022-06-14 13:43:06 -0500

    Added user email verification email on user signup

--- static/js/success.js
+++ static/js/success.js
@@ -1,7 +1,5 @@
-if (window.location.search.includes("session_id") || window.location.pathname.includes("recovery/reset")) {
+if (window.location.search.includes("session_id") || window.location.pathname.includes("recovery/reset") || window.location.pathname === "/signup/verify/success") {
     setTimeout(() => { window.location.replace("/login"); }, 5000);
-} else if (window.location.pathname.includes("signup/success")) {
-    setTimeout(() => { window.location.replace("/me"); }, 10000);
 } else {
     setTimeout(() => { window.location.replace("/"); }, 5000);
 }
