ade3e295f1ca9ab77eca08de7bd8064bdacb10ab
Jorge Gallego <gallegojorge908@gmail.com>
2022-06-14 11:29:48 -0500

    Updated Back End logic for general reports page

--- routing/reports.js
+++ routing/reports.js
@@ -2,34 +2,33 @@ const { Router } = require("express");
 const connection = require("./data/dbconnection");
 const banks = require("./reports/banks");
 const macro = require("./reports/macro");
+const performance = require("./reports/performance");
 const router = Router();
 
 router.get("/", (req, res) => {
-    connection.query("SELECT * FROM Visbanking.AllReports WHERE BankName IS NOT NULL AND State <> '' GROUP BY IDRSSD ORDER BY RAND() LIMIT 0, 15;", (err, results, fields) => {
-        if (err) {
-            res.redirect("/banks");
-        } else {
-            const singleBankReports = results;
-            connection.query("SELECT State, SectionName FROM Visbanking.AllReports WHERE ReportID = 5 ORDER BY RAND();", (err, results, fields) => {
-                if (err) {
-                    res.redirect("/banks");
-                } else {
-                    const macroReports = results.filter(result => result.State!=='');
-                    res.render("reports", {
-                        singleBankReports,
-                        macroReports,
-                        title: "Bank Reports - Visbanking",
-                        path: req.originalUrl,
-                        loggedIn: new Boolean(req.cookies.user && req.cookies.tier && req.cookies.session_id).valueOf()
-                    });
-                }
-            })
-        }
-    });
+	connection.query("SELECT * FROM Visbanking.AllReports ORDER BY RAND();", (err, results, fields) => {
+		if (err) res.redirect("/banks");
+		else {
+			const singleBankReports = results.filter(bank => bank.Type==="Bank").slice(0, 9),
+				macroReports = results.filter(bank => bank.Type==="Macro").slice(0, 9),
+				performanceReports = results.filter(bank => bank.Type==="Performance").slice(0, 9);
+			console.log(performanceReports);
+			res.render("reports", {
+				singleBankReports,
+				macroReports,
+				performanceReports,
+				title: "Reports - Visbanking",
+				path: req.originalUrl,
+				loggedIn: new Boolean(req.cookies.user && req.cookies.tier && req.cookies.session_id).valueOf()
+			});
+		}
+	});
 });
 
 router.use("/bank", banks);
 
 router.use("/macro", macro);
+
+router.use("/performance", performance);
 
 module.exports = router;
