be7bfcfdb02a8b42fea8433c74df0af9c5acce72
Jorge Gallego <gallegojorge908@gmail.com>
2022-06-16 14:46:22 -0500

    Added responsiveness to single HTML report pages

---
+++ static/js/reports/responsive.js
@@ -0,0 +1,16 @@
+const interval = setInterval(() => {
+	let tables = document.querySelectorAll("div.datatables"), tablesOverflowSetCount = 0;
+	let charts = document.querySelectorAll("div.highcharts-container"), chartsOverflowSetCount = 0;
+	tables.forEach(table => {
+		table.style.overflow = "auto";
+		tablesOverflowSetCount++;
+	});
+	charts.forEach(chart => {
+		chart.style.overflowX = "auto";
+		chart.style.width = window.innerWidth-40+"px";
+		chartsOverflowSetCount++;
+	});
+	if ((tablesOverflowSetCount === tables.length) && (chartsOverflowSetCount === charts.length)) {
+		clearInterval(interval);
+	}
+});
