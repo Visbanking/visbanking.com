ca710cea71986458a9cfb0ca814c5f9b54dc3d27
Jorge Gallego <gallegojorge908@gmail.com>
2022-06-21 14:29:50 -0500

    Updated JS script for responsiveness in individual report pages

--- static/js/reports/responsive.js
+++ static/js/reports/responsive.js
@@ -1,16 +1,22 @@
-const interval = setInterval(() => {
-	let tables = document.querySelectorAll("div.datatables"), tablesOverflowSetCount = 0;
-	let charts = document.querySelectorAll("div.highcharts-container"), chartsOverflowSetCount = 0;
-	tables.forEach(table => {
-		table.style.overflow = "auto";
-		tablesOverflowSetCount++;
-	});
-	charts.forEach(chart => {
-		chart.style.overflowX = "auto";
-		chart.style.width = window.innerWidth-40+"px";
-		chartsOverflowSetCount++;
-	});
-	if ((tablesOverflowSetCount === tables.length) && (chartsOverflowSetCount === charts.length)) {
-		clearInterval(interval);
+window.onload = window.onresize = () => {
+	if (window.innerWidth < 768) {
+		const interval = setInterval(() => {
+			let tables = document.querySelectorAll("div.datatables"),
+				tablesOverflowSetCount = 0;
+			let charts = document.querySelectorAll("div.highcharts-container"),
+				chartsOverflowSetCount = 0;
+			tables.forEach((table) => {
+				table.style.overflow = "auto";
+				tablesOverflowSetCount++;
+			});
+			charts.forEach((chart) => {
+				chart.style.overflowX = "auto";
+				chart.style.width = window.innerWidth - 40 + "px";
+				chartsOverflowSetCount++;
+			});
+			if (tablesOverflowSetCount === tables.length && chartsOverflowSetCount === charts.length) {
+				clearInterval(interval);
+			}
+		});
 	}
-});
+};
