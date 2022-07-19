4235e5a004299090c812b05e407134ed00065f99
Jorge Gallego <gallegojorge908@gmail.com>
2022-07-19 11:50:30 -0500

    Created getCache function for redis client

--- data/caching.js
+++ data/caching.js
@@ -6,6 +6,7 @@ const client = redis.createClient(process.env.REDIS_PORT);
 client.connect()
 .catch(console.error);
 
+// Middleware implementation
 const checkCache = async (req, res, next) => {
     if (client.isOpen) {
         const data = await client.get(`visbanking.com${req.originalUrl}`);
@@ -14,11 +15,17 @@ const checkCache = async (req, res, next) => {
     } else next();
 };
 
+// Direct redis client implementation
+const getCache = async (key) => {
+	return await client.get(key);
+}
+
 const setCache = (key, value) => {
     client.isOpen ? client.set(key, value) : null;
 }
 
 module.exports = {
     checkCache,
+	getCache,
     setCache
 };
