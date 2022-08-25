5e2c969d5a0f12babcbe3ca45162a3272803b9a7
Jorge Gallego <gallegojorge908@gmail.com>
2022-08-25 06:55:19 -0500

    Created secure API routing

---
+++ routing/api.js
@@ -0,0 +1,57 @@
+const { Router } = require("express");
+const { verify, JsonWebTokenError } = require("jsonwebtoken");
+const insights = require("./api/insights");
+const router = Router();
+
+router.use((req, res, next) => {
+	if (!req.headers.authorization) {
+		return res.status(400).json({
+			message: "Authorization header not found in request"
+		});
+	} else {
+		const { authorization: auth } = req.headers;
+		if (!auth.startsWith("Bearer ")) {
+			return res.status(400).setHeader("WWW-Authenticate", "error='invalid_request'").json({
+				error: "The authorization method provided is invalid"
+			});
+		}
+		try {
+			const token = verify(auth.slice(7), process.env.JWT_SIGNATURE); 
+			if (!(token.iss && token.iat && token.aud)) {
+				res.status(401).setHeader("WWW-Authenticate", "error='invalid_token', error_description='The access token is malformed'").json({
+					error: "The access token provided is missing required parameters"
+				});
+			} else if (!require("./../data/.apiTokens.json")[token.name]) {
+				res.status(401).setHeader("WWW-Authenticate", "error='invalid_token', error_description='The access token doesn't exist'").json({
+					error: "The access token provided doesn't exist"
+				});
+			} else {
+				const { clientId, clientSecret } = require("./../data/.apiTokens.json")[token.name];
+				if (clientId===token.id && clientSecret===token.secret) next();
+				else {
+					res.status(401).setHeader("WWW-Authenticate", "error='invalid_token', error_description='The access token is invalid'").json({
+						error: "The token's Client ID and/or Client Secret are invalid"
+					});
+				}
+			}
+		} catch (err) {
+			if (err instanceof JsonWebTokenError) {
+				res.status(500).json({
+					error: "Internal Server Error"
+				});
+			}
+		}
+	}
+});
+
+router.use("/insights", insights);
+
+router.use((req, res) => {
+	res.status(404).json({
+		result: {
+			message: "The API endpoint you're looking for doesn't exist"
+		}
+	});
+});
+
+module.exports = router;
---
+++ routing/api/insights.js
@@ -0,0 +1,166 @@
+const { Router } = require("express");
+const { verify } = require("jsonwebtoken");
+const { capitalize } = require("lodash");
+const InsightController = require("../../controllers/Insight");
+const ResourceNotFoundError = require("../../data/errors/ResourceNotFoundError");
+const router = Router();
+
+router.use((req, res, next) => {
+	const apiToken = require("./../../data/.apiTokens.json")[verify(req.headers.authorization.slice(7), process.env.JWT_SIGNATURE).name];
+	if (!Object.keys(apiToken.tokenAccess).includes("insights")) {
+		res.status(403).setHeader("WWW-Authenticate", "error='insufficient_scope', error_description='The access token doesn't have the required scope'").json({
+			error: "The token's doesn't have the required access scope for this resource(s)"
+		});
+	} else if (apiToken.tokenAccess.insights!=="*" && !apiToken.tokenAccess.insights.split(",").includes(req.method.toUpperCase())) {
+		res.status(403).setHeader("WWW-Authenticate", "error='insufficient_scope', error_description='The access token doesn't have the required scope'").json({
+			error: `The token doesn't have the required access scope to request this action (${req.method.toUpperCase()}) for this resource(s)`
+		});
+	} else if (apiToken.tokenAccess.insights==="*" || apiToken.tokenAccess.insights.split(",").includes(req.method.toUpperCase())) next();
+});
+
+router.get("/", (req, res) => {
+	InsightController.getAllInsights(req.query.fields?.split(",").join(" "))
+	.then(insights => {
+		res.status(200)
+		.setHeader("Content-Type", "application/json")
+		.setHeader("Cache-Control", "no-cache")
+		.json({
+			result: insights || []
+		});
+	})
+	.catch(err => {
+		res.status(500).json({
+			error: "Internal Server Error",
+			description: err
+		});
+	});
+});
+
+router.head("/", (req, res) => {
+	InsightController.getAllInsights(req.query.fields?.split(",").join(" "))
+	.then(() => {
+		res
+			.setHeader("Content-Type", "application/json")
+			.setHeader("Cache-Control", "no-cache")
+			.sendStatus(200);
+	})
+	.catch(() => {
+		res.sendStatus(500);
+	});
+});
+
+router.post("/insight", (req, res) => {
+	const insightData = {};
+	for (const key in req.body) insightData[key] = req.body[key];
+	console.log(insightData);
+	InsightController.createNewInsight(insightData)
+	.then(result => {
+		res 
+			.setHeader("Content-Type", "application/json")
+			.setHeader("Cache-Control", "no-cache")
+			.status(201)
+			.json({
+				result: result
+			});
+	})
+	.catch(err => {
+		res.status(500).json({
+			error: "Internal Server Error",
+			description: err
+		});
+	});
+});
+
+router.get("/insight/:insight_id", (req, res) => {
+	InsightController.getInsightById(req.params.insight_id, req.query.fields?.split(",").join(" "))
+	.then(insight => {
+		res
+			.setHeader("Content-Type", "application/json")
+			.setHeader("Cache-Control", "no-cache")
+			.status(insight ? 200 : 204)
+			.json({
+				result: insight || []
+			});
+	})
+	.catch(err => {
+		res.status(500).json({
+			error: "Internal Server Error",
+			description: err
+		});
+	});
+});
+
+router.head("/insight/:insight_id", (req, res) => {
+	InsightController.getInsightById(req.params.insight_id, req.query.fields?.split(",").join(" "))
+	.then(insight => {
+		res
+			.setHeader("Content-Type", "application/json")
+			.setHeader("Cache-Control", "no-cache")
+			.sendStatus(insight ? 200 : 204)
+	})
+	.catch(() => {
+		res.sendStatus(500);
+	});
+});
+
+router.patch("/insight/:insight_id", (req, res) => {
+	const updateOptions = {}
+	for (const key in req.body) updateOptions[key] = req.body[key];
+	InsightController.updateInsightById(req.params.insight_id, updateOptions)
+	.then(result => {
+		res
+			.setHeader("Content-Type", "application/json")
+			.setHeader("Cache-Control", "no-cache")
+			.status(200)
+			.json({
+				result: result
+			});
+	})
+	.catch(err => {
+		if (err instanceof ResourceNotFoundError) {
+			res.status(404).json({
+				error: "Not Found",
+				description: err
+			});
+		} else {
+			res.status(500).json({
+				error: "Internal Server Error",
+				description: err
+			});
+		}
+	});
+});
+
+router.delete("/insight/:insight_id", (req, res) => {
+	InsightController.deleteInsightById(req.params.insight_id)
+	.then(result => {
+		res
+			.setHeader("Content-Type", "application/json")
+			.setHeader("Cache-Control", "no-cache")
+			.status(200)
+			.json({
+				result: result
+			});
+	})
+	.catch(err => {
+		if (err instanceof ResourceNotFoundError) {
+			res.status(404).json({
+				error: "Not Found",
+				description: err
+			});
+		} else {
+			res.status(500).json({
+				error: "Internal Server Error",
+				description: err
+			});
+		}
+	});
+});
+
+router.all("/*", (req, res) => {
+	res.status(501).json({
+		error: `${req.method.toUpperCase()} requests aren't supported for ${req.originalUrl}`
+	});
+});
+
+module.exports = router;
