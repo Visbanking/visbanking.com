45e87c5db1b084a62d85f74a31d9897df630c8b1
Jorge Gallego <gallegojorge908@gmail.com>
2022-09-06 08:23:50 -0500

    Added automatic redirect to new Insight individual pages URLs

--- routing/insights.js
+++ routing/insights.js
@@ -45,4 +45,8 @@ router.get("/:article_id", (req, res) => {
 	});
 });
 
+router.get("/insight/:article_id", (req, res) => {
+	res.redirect(`/insights/${req.params.article_id}`);
+});
+
 module.exports = router;
