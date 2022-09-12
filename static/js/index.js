2ee1c05e325e55a1bd4edbc199025537819aa449
Jorge Gallego <gallegojorge908@gmail.com>
2022-09-12 08:43:02 -0500

    Made content changes to homepage

--- static/js/index.js
+++ static/js/index.js
@@ -9,3 +9,14 @@ document.querySelectorAll("#latest .post").forEach(post => {
 		post.children[post.children.length-1].click();
 	});
 });
+
+setInterval(() => {
+	const images = [ ...document.querySelectorAll("#dashboards img") ];
+	const indicators = [ ...document.querySelectorAll("#dashboards .indicator i.bi-dot") ];
+	const currentImageIndex = images.indexOf(document.querySelector("#dashboards img.active"));
+	const nextImageIndex = (currentImageIndex===images.length-1) ? 0 : currentImageIndex+1;
+	images[currentImageIndex].classList.remove("active");
+	indicators[currentImageIndex].classList.remove("active");
+	images[nextImageIndex].classList.add("active");
+	indicators[nextImageIndex].classList.add("active");
+}, 10000);
