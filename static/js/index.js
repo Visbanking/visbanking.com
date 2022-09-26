832b5e8907d413c6bc0f6bf40b8d45be4063c368
Jorge Gallego <gallegojorge908@gmail.com>
2022-09-26 10:49:36 -0500

    Implemented new homepage design

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,7 @@
-document.querySelectorAll("#featured .post").forEach(post => {
-	post.addEventListener("click", () => {
-		post.children[post.children.length-1].click();
-	});
-});
-
-document.querySelectorAll("#latest .post").forEach(post => {
-	post.addEventListener("click", () => {
-		post.children[post.children.length-1].click();
-	});
-});
-
 setInterval(() => {
-	const images = [ ...document.querySelectorAll("#dashboards img") ];
-	const indicators = [ ...document.querySelectorAll("#dashboards .indicator i.bi-dot") ];
-	const currentImageIndex = images.indexOf(document.querySelector("#dashboards img.active"));
+	const images = [ ...document.querySelectorAll("#intro img") ];
+	const currentImageIndex = images.indexOf(document.querySelector("#intro img.active"));
 	const nextImageIndex = (currentImageIndex===images.length-1) ? 0 : currentImageIndex+1;
 	images[currentImageIndex].classList.remove("active");
-	indicators[currentImageIndex].classList.remove("active");
 	images[nextImageIndex].classList.add("active");
-	indicators[nextImageIndex].classList.add("active");
-}, 10000);
+}, 5000);
