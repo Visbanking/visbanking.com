a68e8cf781a66542639be91b8c086f36ba346932
Jorge Gallego <gallegojorge908@gmail.com>
2022-09-28 08:26:10 -0500

    Upgraded Analytics from UA to GA4

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,5 +1,8 @@
+// Google Analytics 4
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() {
+	dataLayer.push(arguments);
+}
 gtag("js", new Date());
 
-gtag("config", "UA-214131359-1");
+gtag("config", "G-MJJWFHMT7Z");
