631697d691ab7eac439cc5299c44f3364dc80525
Jorge Gallego <gallegojorge908@gmail.com>
2022-10-31 16:02:26 -0500

    Integrated winston logger instance into MySQL database connection authentication script

--- data/database/usersDatabase.js
+++ data/database/usersDatabase.js
@@ -1,13 +1,19 @@
 const { Sequelize } = require("sequelize");
+const infoLogger = require("../log/info.log");
 require("dotenv").config();
 
-const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const connection = new Sequelize({
+	dialect: "mysql",
 	host: process.env.DB_HOST,
-	dialect: "mysql"
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	port: 3306
 });
 
 (async () => {
 	await connection.authenticate();
+	infoLogger.info("Database Connection Successful");
 })();
 
 module.exports = connection;
